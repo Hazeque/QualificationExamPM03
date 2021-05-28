f06824ddb680f25c5b393eac689e238685414574
Hazeque <64709171+Hazeque@users.noreply.github.com>
2021-05-28 09:49:37 +0300

    Add files via upload
    Добавлен метод "Read"

--- QualExam/src/QualExam/App.cs
+++ QualExam/src/QualExam/App.cs
@@ -35,4 +35,27 @@ namespace QualExam
             }
         }
     }
+    public static App[] Read(App[] Apps)
+    {
+        for (int i = 0; i < Apps.Length; i++)
+        {
+            Apps[i] = new App();
+            Console.WriteLine("Введите наименование программы");
+            Apps[i].Name = Console.ReadLine();
+            Console.WriteLine("Введите производителя программы");
+            Apps[i].Creator = Console.ReadLine();
+            Console.WriteLine("Введите цену программы");
+            Apps[i].Price = Convert.ToDouble(Console.ReadLine());
+        }
+        return Apps;
+    }
+
+      //for (int i = 0; i<Apps.Length; i++)
+      //  {
+      //      for (int j = 0; j<Apps.Length; j++)
+      //      {
+
+      //      }
+      //  }
 }
+
