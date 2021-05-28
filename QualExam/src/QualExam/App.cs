bbc93d9a3496f72e7d2aee5823b4d7ea2ac2b00c
Hazeque <64709171+Hazeque@users.noreply.github.com>
2021-05-28 09:30:12 +0300

    Add files via upload
    Добавлен класс "App"

---
+++ QualExam/src/QualExam/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualExam
+{
+    class App
+    {
+        string name;
+        string creator;
+        double price;
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Creator
+        {
+            get => this.creator;
+            set
+            {
+                this.creator= value;
+            }
+        }
+        public double Price
+        {
+            get => this.price;
+            set
+            {
+                this.price = value;
+            }
+        }
+    }
+}
